a96e2bcc755461558dd99f89e780d45a2b7efb09
Tuula Flynn <62618067+tuulaflynn@users.noreply.github.com>
2024-03-03 11:43:08 +0000

    Console in front-end is showing desired data.

--- client/restaurant-data-web-interface.js
+++ client/restaurant-data-web-interface.js
@@ -5,9 +5,20 @@ function getRestaurants() {
                 throw new Error('Response was not ok');
             }
             console.log("Success")
-            return res.json();
+            return res.json(); // a json array is received from the enpdoint in 'res' and parsed into a JavaScript object using json().
         })
-        .then(data => { console.log(data); })
+        .then(data => {
+            data.forEach(restaurant => {
+                console.log(restaurant.name);
+                console.log(restaurant.cuisines);
+                // Nested value in received resturant object is a JSON string, parse these into a javascript objects
+                let ratingJsObject = JSON.parse(restaurant.ratings.rating);
+                let addressJsObject = JSON.parse(restaurant.address.address);
+                console.log(ratingJsObject);
+                console.log(addressJsObject);
+                console.log("xxxxxxxxxxxxxxxxxxxxx")
+            });
+        })
         .catch(e => {
             console.error('There was a problem with the fetch operation:', e);
         });
